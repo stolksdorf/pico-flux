c81da95d8f51eabb8c7916ddcb3af4ecd9dc0a56
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-07-01 08:06:52 -0400

    updating store with opts

--- src/store.js
+++ src/store.js
@@ -1,35 +1,39 @@
 const EventEmitter = require('events');
 
-const sameArray = (a, b) => {
+const sameArray = (a, b)=>{
 	if(a.length !== b.length) return false;
-	return a.every((val, idx) => val === b[idx]);
+	return a.every((val, idx)=>val === b[idx]);
 };
 
-module.exports = (setters = {}, getters = {}) => {
+module.exports = (setters = {}, getters = {}, _opts = {})=>{
+	const opts = Object.assign({ event : 'update', cache : false }, _opts);
 	let memoize = {};
 	const store = {
 		emitter : new EventEmitter(),
-		emit    : (evt = 'update') => store.emitter.emit(evt),
-		getter  : (name, func) => {
-			store[name] = (...args) => {
-				if(memoize[name] && sameArray(memoize[name].args, args)) return memoize[name].result;
+		emit    : (evt = opts.event)=>store.emitter.emit(evt),
+		getter  : (name, func)=>{
+			if(!opts.cache) return store[name] = func;
+			store[name] = (...args)=>{
+				if(memoize[name] && sameArray(memoize[name].args, args)){
+					return memoize[name].result;
+				}
 				const result = func(...args);
 				memoize[name] = { args, result };
 				return result;
 			};
 		},
-		setter : (name, func) => {
-			store[name] = (...args) => {
+		setter : (name, func)=>{
+			store[name] = (...args)=>{
 				const result = func(...args);
 				if(result !== false){
 					memoize = {};
-					store.emitter.emit('update');
+					store.emitter.emit(opts.event);
 				}
 				return result;
 			};
 		},
 	};
-	Object.keys(setters).map((name) => store.setter(name, setters[name]));
-	Object.keys(getters).map((name) => store.getter(name, getters[name]));
+	Object.keys(setters).map((name)=>store.setter(name, setters[name]));
+	Object.keys(getters).map((name)=>store.getter(name, getters[name]));
 	return store;
 };
--- test/store.test.js
+++ test/store.test.js
@@ -4,12 +4,12 @@ const Store = require('../src/store.js');
 //test = test.only();
 
 
-test('getting the same value is cached', (t) => {
+test('getting the same value is cached', (t)=>{
 	let updateCount = 0;
-	const store = Store({}, { add : (a, b) => {
+	const store = Store({}, { add : (a, b)=>{
 		updateCount++;
 		return a + b;
-	} });
+	} }, { cache : true });
 
 	t.is(store.add(3, 4), 7);
 	t.is(updateCount, 1);
@@ -22,14 +22,14 @@ test('getting the same value is cached', (t) => {
 });
 
 
-test('if a setter is called, memoziation is flushed', (t) => {
+test('if a setter is called, memoziation is flushed', (t)=>{
 	let updateCount = 0, value = 0;
 	const store = Store({
-		set : (newVal) => value = newVal,
-	}, { get : () => {
+		set : (newVal)=>value = newVal,
+	}, { get : ()=>{
 		updateCount++;
 		return value;
-	} });
+	} }, { cache : true });
 
 	t.is(store.get(), value);
 	t.is(updateCount, 1);
@@ -45,17 +45,17 @@ test('if a setter is called, memoziation is flushed', (t) => {
 	t.is(updateCount, 2);
 });
 
-test('if a setter returns false, no-op', (t) => {
+test('if a setter returns false, no-op', (t)=>{
 	let updateCount = 0, value = 0;
 	const store = Store({
-		set : (newVal) => {
+		set : (newVal)=>{
 			value = newVal;
 			return newVal;
 		},
-	}, { get : () => {
+	}, { get : ()=>{
 		updateCount++;
 		return value;
-	} });
+	} }, { cache : true });
 
 	store.get();
 	t.is(updateCount, 1);
@@ -71,13 +71,13 @@ test('if a setter returns false, no-op', (t) => {
 	t.is(updateCount, 2, 'set(false) is a no-op, so .get() should still be cached');
 });
 
-test('update event is emitted when a setter is called', (t) => {
+test('update event is emitted when a setter is called', (t)=>{
 	let updateCount = 0;
 	const store = Store({
-		set : (newVal) => newVal,
+		set : (newVal)=>newVal,
 	});
 
-	store.emitter.on('update', () => updateCount++);
+	store.emitter.on('update', ()=>updateCount++);
 
 	store.set(5);
 	t.is(updateCount, 1);
